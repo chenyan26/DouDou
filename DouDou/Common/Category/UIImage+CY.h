ca6af7e32a5374b1d1b934df42531c1257c14307
sgt39007 <sgt39007@163.com>
2016-07-28 10:24:01 +0800

    init

---
+++ DouDou/Common/Category/UIImage+CY.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+CY.h
+//  DouDou
+//
+//  Created by echo on 7/28/16.
+//  Copyright © 2016 caratel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (CY)
+
++ (UIImage *)imageWithColor:(UIColor *)color;
+
+@end
---
+++ Pods/Target Support Files/Pods-DouDou/Pods-DouDou-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DouDouVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DouDouVersionString[];
+
